1b14df6551437ef7d47ff621943951899ce7bc29
iyaz07 <moseseniade@yahoo.com>
2024-10-29 23:42:05 +0000

    Add Nosql Mongo

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,9 +8,16 @@ Returns the new _id
 """
 
 
-import pymongo
-
-
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection"""
-    return mongo_collection.insert_one(kwargs)
+    """
+    Inserts a new document into the specified MongoDB collection with attributes given as keyword arguments.
+
+    Args:
+        mongo_collection: The pymongo collection object to insert the document into.
+        **kwargs: Key-value pairs representing the attributes of the document to insert.
+
+    Returns:
+        The _id of the newly inserted document.
+    """
+    args = mongo_collection.insert_one(kwargs)
+    return args.inserted_id
